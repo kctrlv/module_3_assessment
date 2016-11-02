64d9fa159c7ef1d67e18eefaa7c03d2d2c130a28
David Davydov <kctrlv@gmail.com>
2016-11-02 11:59:03 -0600

    integrate vcr

--- spec/features/stores_lookup_spec.rb
+++ spec/features/stores_lookup_spec.rb
@@ -2,30 +2,33 @@ require 'rails_helper'
 
 RSpec.feature 'Stores Lookup' do
   scenario 'User can search for stores in a location' do
+    VCR.use_cassette("store_search") do
+      # As a user
+      # When I visit "/"
+      visit '/'
+      # And I fill in a search box with "80202" and click "search"
+      fill_in "s", with: '80202'
+      click_button 'Search'
+      # Then my current path should be "/search" (ignoring params)
+      expect(current_path).to eq("/search")
+      # And I should see stores within 25 miles of 80202
+      expect(page).to have_content("Stores within 25 miles of 80202")
+      # And I should see a message that says "17 Total Stores"
+      expect(page).to have_content("17 Total Stores")
+      # And I should see exactly 15 results
+      expect(page).to have_content("Showing the first 15 results:")
+      expect(page.all(".store").count).to eq(15)
+      # And I should see the long name, city, distance, phone number and store type for each of the 15 results
+      within('.stores') do
+        expect(page).to have_content('Best Buy Mobile - Cherry Creek Shopping Center')
+        expect(page).to have_content('City: Denver')
+        expect(page).to have_content('Distance: 3.25 miles')
+        expect(page).to have_content('Phone: 303-270-9189')
+        expect(page).to have_content('Store Type: Mobile')
+      end
 
-    # As a user
-    # When I visit "/"
-    visit '/'
-    # And I fill in a search box with "80202" and click "search"
-    fill_in "s", with: '80202'
-    click_button 'Search'
-    # Then my current path should be "/search" (ignoring params)
-    expect(current_path).to eq("/search")
-    # And I should see stores within 25 miles of 80202
-    expect(page).to have_content("Stores within 25 miles of 80202")
-    # And I should see a message that says "17 Total Stores"
-    expect(page).to have_content("17 Total Stores")
-    # And I should see exactly 15 results
-    expect(page).to have_content("Showing the first 15 results:")
-    expect(page.all(".store").count).to eq(15)
-    # And I should see the long name, city, distance, phone number and store type for each of the 15 results
-    within('.stores') do
-      expect(page).to have_content('Best Buy Mobile - Cherry Creek Shopping Center')
-      expect(page).to have_content('City: Denver')
-      expect(page).to have_content('Distance: 3.25 miles')
-      expect(page).to have_content('Phone: 303-270-9189')
-      expect(page).to have_content('Store Type: Mobile')
     end
+
 
     # The name will be a link in the next story:
     #
