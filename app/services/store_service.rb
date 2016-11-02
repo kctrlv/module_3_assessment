d4de84761acf34d11203b25161ac24d496e88f60
David Davydov <kctrlv@gmail.com>
2016-11-02 11:48:44 -0600

    working

--- app/services/store_service.rb
+++ app/services/store_service.rb
@@ -7,20 +7,9 @@ class StoreService
       apiKey: ENV['api_key'],
       show: 'longName,city,distance,phone,storeType'
     }
+
     res = conn.get "/v1/stores(area(#{zipcode},25))", parameters
     results = JSON.parse(res.body)
     results['stores']
   end
-
-  def initialize(zipcode)
-    @conn = Faraday.new(url: "https://api.bestbuy.com")
-    @parameters = {
-      format: 'json',
-      pageSize: 50,
-      apiKey: ENV['api_key'],
-      show: 'longName,city,distance,phone,storeType'
-    }
-  end
-
-
 end
